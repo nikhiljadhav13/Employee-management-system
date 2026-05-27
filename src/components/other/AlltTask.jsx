6c038164251b18e2f0acbb36d8e695a29bb87499
nikhiljadhav13 <115921099+nikhiljadhav13@users.noreply.github.com>
2026-05-27 16:06:49 +0530

    created logout featur

--- src/components/other/AlltTask.jsx
+++ src/components/other/AlltTask.jsx
@@ -1,38 +1,32 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../context/AuthProvider'
 
-const AlltTask = () => {
+const AlltTask = (data) => {
+ const authdata = useContext(AuthContext)
   return (
-    <div id='alltask' className='p-5 rounded h-40 overflow-auto bg-[#1c1c1c] mt-4'>
-      <div className='flex mb-2 justify-between bg-blue-400 py-3 px-4 rounded '>
-        <h2>Nikhil</h2>
-        <h3>Make a UI design</h3>
-        <h3>Status</h3>
+    <div id='alltask' className='p-5 rounded h-44 overflow-auto bg-[#1c1c1c] mt-4'>
+      <div className='flex mb-2 justify-between bg-red-400 py-3 px-4 rounded '>
+        <h2 className='w-1/5 bg-amber-300 '>name</h2>
+        <h3  className='w-1/5 bg-amber-300 '>Make a UI design</h3>
+        <h3 className='w-1/5 bg-amber-300 '>Status</h3>
+        <h3 className='w-1/5 bg-amber-300 '>Status</h3>
+        <h3 className='w-1/5 bg-amber-300 '>Status</h3>
+
+      
       </div>
-       <div className='flex mb-2  justify-between bg-yellow-400 py-3 px-4 rounded '>
-        <h2>Nikhil</h2>
-        <h3>Make a UI design</h3>
-        <h3>Status</h3>
+      {authdata.employees.map((elem)=>{
+
+        return <div className='flex mb-2 justify-between bg-blue-400 py-3 px-4 rounded '>
+        <h2 className='w-1/5 bg-amber-300 '>{elem.firstname}</h2>
+        <h3  className='w-1/5 bg-amber-300 '>Make a UI design</h3>
+        <h3 className='w-1/5 bg-amber-300 '>Status</h3>
+        <h3 className='w-1/5 bg-amber-300 '>Status</h3>
+        <h3 className='w-1/5 bg-amber-300 '>Status</h3>
+
+      
       </div>
-       <div className='flex mb-2 justify-between bg-pink-400 py-3 px-4 rounded '>
-        <h2>Nikhil</h2>
-        <h3>Make a UI design</h3>
-        <h3>Status</h3>
-      </div>
-       <div className='flex mb-2 justify-between bg-green-400 py-3 px-4 rounded '>
-        <h2>Nikhil</h2>
-        <h3>Make a UI design</h3>
-        <h3>Status</h3>
-      </div>
-       <div className='flex mb-2 justify-between bg-purple-400 py-3 px-4 rounded '>
-        <h2>Nikhil</h2>
-        <h3>Make a UI design</h3>
-        <h3>Status</h3>
-      </div>
-       <div className='flex mb-2 justify-between bg-red-400 py-3 px-4 rounded '>
-        <h2>Nikhil</h2>
-        <h3>Make a UI design</h3>
-        <h3>Status</h3>
-      </div>
+      })}
+       
     </div>
   )
 }
